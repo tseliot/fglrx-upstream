51fbf597e22845bd600b7988cb56af78a6e519a9
Mario Limonciello <superm1@ubuntu.com>
2007-12-19 03:52:29 -0600

    Add LSB release detection to Ubuntu

--- Ubuntu/ati-packager.sh
+++ Ubuntu/ati-packager.sh
@@ -26,17 +26,17 @@ umask 002
 #Purpose: lists distribution supported packages
 getSupportedPackages()
 {
-	echo $DAPPER $EDGY $FEISTY $GUTSY $HARDY
+    echo $DAPPER $EDGY $FEISTY $GUTSY $HARDY
 }
 
 makeChangelog()
 {
     printf "%b\n" "fglrx-installer (${DRV_RELEASE}-`./ati-packager-helper.sh --release`) experimental; urgency=low\n" \
-	> ${TmpDrvFilesDir}/debian/changelog
+    > ${TmpDrvFilesDir}/debian/changelog
     printf "%b\n" "  * new release\n" \
-	>> ${TmpDrvFilesDir}/debian/changelog
+    >> ${TmpDrvFilesDir}/debian/changelog
     printf "%b\n" " -- ${DEBEMAIL}  `date --rfc-822`\n" \
-	>> ${TmpDrvFilesDir}/debian/changelog
+    >> ${TmpDrvFilesDir}/debian/changelog
 }
 
 
@@ -45,13 +45,13 @@ makeChangelog()
 buildPackage()
 {
     export X_NAME=$1                    # Well known X or distro name (exported for dpkg rules)
-    RelDistroDir=`dirname $0`			# Relative path to the distro directory
+    RelDistroDir=`dirname $0`           # Relative path to the distro directory
     AbsDistroDir=`cd ${RelDistroDir} 2>/dev/null && pwd` # Absolute path to the distro directory
-    InstallerRootDir=`pwd`    			# Absolute path of the <installer root> directory
-    TmpPkgBuildOut="/tmp/pkg_build.out"	        # Temporary file to output diagnostics of the package build utility
+    InstallerRootDir=`pwd`              # Absolute path of the <installer root> directory
+    TmpPkgBuildOut="/tmp/pkg_build.out"         # Temporary file to output diagnostics of the package build utility
     TmpDrvFilesDir=`mktemp -d -t fglrx.XXXXXX`  # Temporary directory to merge files from the common and x* directories
 
-    #Detect target architechture if not set  
+    #Detect target architecture if not set
     if [ -z "$ARCH" ]; then
         ARCH=`dpkg-architecture -qDEB_HOST_ARCH`
     fi
@@ -66,18 +66,20 @@ buildPackage()
         feisty|7.04) X_DIR=x710; X_NAME=feisty;;
         gutsy|7.10)  X_DIR=x710; X_NAME=gutsy;;
         hardy|8.04)  X_DIR=x710; X_NAME=hardy;;
-        *) echo "Error: invalid package name passed to --buildpkg" ; exit 1 ;;
+        *)
+        #Automatically detect
+        echo "Error: invalid package name passed to --buildpkg" ; exit 1 ;;
     esac
 
     case ${ARCH} in
-	amd64) ARCH_DIR=x86_64; X_DIR=${X_DIR}_64a;;
-	i386)  ARCH_DIR=x86;;
+    amd64) ARCH_DIR=x86_64; X_DIR=${X_DIR}_64a;;
+    i386)  ARCH_DIR=x86;;
         *) echo "Error: unsupported architecture: ${ARCH}" ; exit 1 ;;
     esac
 
     PKG_BUILD_UTIL="dpkg-buildpackage" # Package build utility
     PKG_BUILD_OPTIONS="-a${ARCH} -b -nc -tc -uc -d"
-    EXIT_CODE=0	# Script exit code
+    EXIT_CODE=0 # Script exit code
 
     if [ "$USER" != "root" ]; then
         if [ -x "`which fakeroot`" ]; then
@@ -87,7 +89,7 @@ buildPackage()
             exit 1
         fi
     fi
- 
+
     #Merge files from different source directories
     cp -f -R ${InstallerRootDir}/${X_DIR}/* ${TmpDrvFilesDir}
     cp -f -R ${InstallerRootDir}/arch/${ARCH_DIR}/* ${TmpDrvFilesDir}
@@ -95,44 +97,44 @@ buildPackage()
 
     if [ "$ARCH" = "amd64" ]; then
         cp -f -R ${InstallerRootDir}/arch/x86/usr/X11R6/lib \
-		${TmpDrvFilesDir}/usr/X11R6/
+        ${TmpDrvFilesDir}/usr/X11R6/
     fi
 
     # Merge package files from the appropriate directories
-	chmod -R u+w ${AbsDistroDir}
-	cp -f -R ${AbsDistroDir}/dists/${X_NAME} ${TmpDrvFilesDir}/debian
+    chmod -R u+w ${AbsDistroDir}
+    cp -f -R ${AbsDistroDir}/dists/${X_NAME} ${TmpDrvFilesDir}/debian
     cp -f -R ${AbsDistroDir}/module ${TmpDrvFilesDir}
 
     # generate a temporary changelog with version information
     makeChangelog
-    
+
     #Build the package
-    cd ${TmpDrvFilesDir}    
+    cd ${TmpDrvFilesDir}
     ${PKG_BUILD_UTIL} ${PKG_BUILD_OPTIONS} > ${TmpPkgBuildOut} 2>&1
 
     if [ $? -eq 0 ]; then
         #String containing info where the package was created
         PACKAGE_FILES=`grep 'building package .* in .*\.deb' ${TmpPkgBuildOut} | sed -e 's/.*in \`\(.*\.deb\).*/\1/'`
 
-    	AbsInstallerParentDir=`cd ${InstallerRootDir}/.. 2>/dev/null && pwd` 	# Absolute path to the installer parent directory
+        AbsInstallerParentDir=`cd ${InstallerRootDir}/.. 2>/dev/null && pwd`    # Absolute path to the installer parent directory
 
         for i in ${PACKAGE_FILES}; do
-            mv $i ${AbsInstallerParentDir}	# move the created package to the directory where the self-extracting driver archive is located
+            mv $i ${AbsInstallerParentDir}  # move the created package to the directory where the self-extracting driver archive is located
             echo "Package ${AbsInstallerParentDir}/`basename ${i}` has been successfully generated"
         done
-	
+
         mv ../fglrx-installer*.changes ${AbsInstallerParentDir}
     else
         echo "Package build failed!"
         echo "Package build utility output:"
-        cat ${TmpPkgBuildOut} 
+        cat ${TmpPkgBuildOut}
         EXIT_CODE=1
     fi
-    
+
     #Clean-up
     rm -rf ${TmpDrvFilesDir} > /dev/null
     rm -f ${TmpPkgBuildOut} > /dev/null
-    
+
     exit ${EXIT_CODE}
 }
 
@@ -143,30 +145,47 @@ action=$1
 
 case "${action}" in
 --get-supported)
-    getSupportedPackages   
+    getSupportedPackages
     ;;
 --buildpkg)
+    #First determine if we are explicitly calling a release build
     package=$2
-    if [ "${package}" != "" ]
+    support_flag=false
+    for supported_list in `getSupportedPackages`
+    do
+        if [ "${supported_list}" = "${package}" ]
+        then
+            support_flag=true
+            break
+        fi
+    done
+    #If we are explicitly trying to build something that isn't supported
+    if [ "${package}" != "Ubuntu" ]
     then
-        support_flag=false
+        echo ${package} "is invalid.  Attempting automatic detection."
+    fi
+    #If we haven't explicitly called, or failed to type something coherent
+    #automatically detect
+    if [ "${support_flag}" != "true" ]
+    then
+        package=`lsb_release -s -c`
         for supported_list in `getSupportedPackages`
         do
             if [ "${supported_list}" = "${package}" ]
             then
                 support_flag=true
+                echo "Automatically detected" ${package}
                 break
             fi
         done
-        if [ "${support_flag}" = "true" ]
-        then
-            buildPackage ${package}
-        else
-            echo "Requested package is not supported."
-        fi
+    fi
+    if [ "${support_flag}" = "true" ]
+    then
+        buildPackage ${package}
     else
-		echo "Please provide package name"
-    fi    
+        echo "Unable to build package for" ${package}
+        exit 1
+    fi
     exit 0
     ;;
 *|--*)
