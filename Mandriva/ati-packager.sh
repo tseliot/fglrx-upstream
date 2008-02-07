da28ed87088e01c0fde21ccd62cfb14b8cf5d0a4
Anssi Hannula <anssi@mandriva.org>
2008-02-07 12:52:11 +0200

    update Mandriva packaging directory
    Replace Mandriva ati.spec with a rewritten fglrx.spec from Mandriva
    SVN, and update ati-packager.sh accordingly. This makes the Mandriva
    package build actually work. There is no longer need to specify version
    when building a Mandriva package, "--buildpkg Mandriva" works fine.
    
    See fglrx.spec for the long list of differences in the rewritten
    version.

--- Mandriva/ati-packager.sh
+++ Mandriva/ati-packager.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Purpose
-#   Sample packaging script
+#   Mandriva ATI packaging script
 #
 # Usage
 #   See README.distro document
@@ -12,42 +12,6 @@ export LC_ALL=C
 # List of supported distributions.
 SuppDistro="2006 2007 2008"
 
-#Function: getXVersion()
-#Purpose : returns the Xorg/Xfree version for a given distribution
-getXVersion()
-{
-	case "$1" in
-		2005*) echo "6.8.0"
-			;;
-		2006*) echo "6.9.0"
-			;;
-		2007* | 2008*) echo "7.1.0"
-			;;
-		*)	echo "Err"
-			;;
-	esac
-}
-
-#Function: getXDir()
-#Purpose : returns the Xorg/Xfree directory for a given arch and X version
-# first arg : X version, second arg : arch
-getXDir()
-{
-	case "$1" in
-		6.8.0)	MyDir="x680"
-			;;
-		6.9.0)  MyDir="x690"
-			;;
-		7.1.0)  MyDir="x710"
-			;;
-	esac
-	
-	if [ "$2" == "x86_64" ]; then
-	    MyDir="${MyDir}_64a"
-	fi
-
-	echo "${MyDir}"
-}
 #Function: getSupportedPackages()
 #Purpose: lists distribution supported packages
 getSupportedPackages()
@@ -70,22 +34,8 @@ buildPackage()
     RelDistroDir=`dirname $0`					# Relative path to the distro directory
     AbsDistroDir=`cd ${RelDistroDir} 2>/dev/null && pwd`	# Absolute path to the distro directory
     InstallerRootDir=`pwd`    					# Absolute path of the <installer root> directory
-    RpmDirs="BUILD SPECS SOURCES RPMS SRPMS"
+    RpmDirs="BUILD SPECS SOURCES RPMS SRPMS tmp"
     Arch=`uname -m`						# Architecture
-
-    if [ $Arch == "x86_64" ]; then
-	    PackName="fglrx64"
-    else
-	    PackName="fglrx"
-    fi
-    XorgVersion=`getXVersion ${DistroName}`
-    AtiXorgVersion=`getXDir ${XorgVersion} ${Arch}`
-    Version=`./ati-packager-helper.sh --version`
-    RpmRoot=`mktemp -d ${TMPDIR:=/tmp}/ati.XXXXXX`		# Rpm TopDir
-    TmpPkgBuildDir="${RpmRoot}/BUILD"
-    TmpPkgBuildOut="${RpmRoot}/pkg_build.out"			# Temporary file to output diagnostics of the 
-    TmpPkgSpec="${RpmRoot}/SPECS/ati.spec"			# Spec file
-    EXIT_CODE=0							# Script exit code
 
     # before trying to build something, check that we have at least rpm-build
     if [ ! -x /usr/bin/rpmbuild ]; then
@@ -93,45 +43,36 @@ buildPackage()
 	    exit 1
     fi
 
+    RpmRoot=`mktemp -d ${TMPDIR:=/tmp}/ati.XXXXXX`		# Rpm TopDir
+    TmpPkgBuildDir="${RpmRoot}/BUILD"
+    TmpPkgBuildOut="${RpmRoot}/pkg_build.out"			# Temporary file to output diagnostics of the 
+    TmpPkgSpec="${RpmRoot}/SPECS/fglrx.spec"			# Spec file
+    EXIT_CODE=0							# Script exit code
+
     # create directories
     for d in ${RpmDirs}; do
 	    mkdir -p ${RpmRoot}/$d
     done
-    mkdir -p ${TmpPkgBuildDir}/${PackName}-${XorgVersion}-${Version}
 
     # copy spec file and binaries
-    cp ${AbsDistroDir}/ati.spec ${TmpPkgSpec}
-    if [ $Arch == "x86_64" ]; then
-	    cp -a ${InstallerRootDir}/arch/x86_64/* ${TmpPkgBuildDir}/${PackName}-${XorgVersion}-${Version}
-    else
-	    cp -a ${InstallerRootDir}/arch/x86/* ${TmpPkgBuildDir}/${PackName}-${XorgVersion}-${Version}
-    fi
-
-    # copy atieventsd initscript
-    mkdir -p ${TmpPkgBuildDir}/${PackName}-${XorgVersion}-${Version}/etc/init.d
-    cp ${AbsDistroDir}/atieventsd.init ${TmpPkgBuildDir}/${PackName}-${XorgVersion}-${Version}/etc/init.d/atieventsd
-    
-    #Substitute variables in the specfile
-    sed -i -f - ${TmpPkgSpec}  <<END_SED_SCRIPT
-s!@xorg_version@!${XorgVersion}!
-s!@ati_xorg_version@!${AtiXorgVersion}!
-s!@version@!`./ati-packager-helper.sh --version`!
-s!@release@!`./ati-packager-helper.sh --release`!
-s!@packname@!${PackName}!
-s!@vendor@!`./ati-packager-helper.sh --vendor`!
-s!@mail@!`./ati-packager-helper.sh --url`!
-s!@date@!`date +"%a %b %d %Y"`!
-END_SED_SCRIPT
- 
-
-    # Merge files from different source directories
-    cp -a ${InstallerRootDir}/${AtiXorgVersion}/* ${TmpPkgBuildDir}/${PackName}-${XorgVersion}-${Version}
-    cp -a ${InstallerRootDir}/common/* ${TmpPkgBuildDir}/${PackName}-${XorgVersion}-${Version}
+    cp ${AbsDistroDir}/fglrx.spec ${TmpPkgSpec}
 
     #Build the package
-    rpm -bb --define "_topdir ${RpmRoot}" --define "_tmppath ${RpmRoot}/BUILD/" ${TmpPkgSpec} > ${TmpPkgBuildOut} 2>&1
-
-
+    rpm -bb --with ati \
+	--define "_topdir ${RpmRoot}" \
+	--define "_tmppath ${RpmRoot}/tmp" \
+	--define "_builddir ${RpmRoot}/BUILD" \
+	--define "_rpmdir ${RpmRoot}/RPMS" \
+	--define "_sourcedir ${AbsDistroDir}" \
+	--define "version $(./ati-packager-helper.sh --version)" \
+	--define "rel $(./ati-packager-helper.sh --release)" \
+	--define "ati_dir ${InstallerRootDir}" \
+	--define "distsuffix amd.mdv" \
+	--define "vendor $(./ati-packager-helper.sh --vendor)" \
+	--define "packager $(./ati-packager-helper.sh --vendor)" \
+	--define "mdkversion ${DistroName}00" \
+	--define "mandriva_release ${DistroName}" \
+	${TmpPkgSpec} > ${TmpPkgBuildOut} 2>&1
 
     #Retrieve the absolute path to the built package
     if [ $? -eq 0 ]; then
@@ -170,27 +111,39 @@ case "${action}" in
     getSupportedPackages   
     ;;
 --buildpkg)
+    #First determine if we are explicitly calling a release build
     package=$2
-    if [ "${package}" != "" ]
+    support_flag=false
+    for supported_list in `getSupportedPackages`
+    do
+        if [ "${supported_list}" = "${package}" ]
+        then
+            support_flag=true
+            break
+        fi
+    done
+    #If we haven't explicitly called, or failed to type something coherent
+    #automatically detect
+    if [ "${support_flag}" != "true" ]
     then
-        support_flag=false
+        package=$(cat /etc/version | cut -d. -f1)
         for supported_list in `getSupportedPackages`
         do
             if [ "${supported_list}" = "${package}" ]
             then
                 support_flag=true
+                echo "Automatically detected" ${package}
                 break
             fi
         done
-        if [ "${support_flag}" = "true" ]
-        then
-            buildPackage ${package}
-        else
-            echo "Requested package is not supported."
-        fi
+    fi
+    if [ "${support_flag}" = "true" ]
+    then
+        buildPackage ${package}
     else
-		echo "Please provide package name"
-    fi    
+        echo "Unable to build package for" ${package}
+        exit 1
+    fi
     exit 0
     ;;
 *|--*)
