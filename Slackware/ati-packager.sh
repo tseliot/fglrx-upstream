fed9e00e6d07da34448f5dfd4b7baf0c572b5054
Emanuele Tomasi <tomasi@cli.di.unipi.it>
2009-04-02 17:06:58 +0200

    Slackware:
      * MIT license in now adopted

--- Slackware/ati-packager.sh
+++ Slackware/ati-packager.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-# by Emanuele Tomasi <tomasi@cli.di.unipi.it>
-#    Ezio Ghibaudo   <ekxius@gmail.com>
-#    Federico Rota   <federico.rota01@gmail.com>
+
+# Copyright (c) 2009 Emanuele Tomasi, Ezio Ghibaudo, Federico Rota
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
 
 # Imposta la lingua dei messaggi del builder.
 # Crea l'array MESSAGE usato per le stampe dei messaggi in output
